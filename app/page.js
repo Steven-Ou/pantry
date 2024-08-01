91bb50ec35e55fcb7f67ed57fb7e6de4734474c4
Steven-Ou <osteve425@gmail.com>
2024-08-01 15:11:12 -0400

    Upgrading in process

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import {Box,Stack, Typography} from "@mui/material";
+import { lightBlue } from "@mui/material/colors";
 
 const item = [ 'tomato','potato','onion','garlic','carrot','lettuce','kale','cucumber']
 
@@ -10,9 +11,23 @@ export default function Home() {
       height="100vh"
       display = {'flex'}
       justifyContent={'center'}
+      flexDirection={'column'}
       alignItems={'center'}
     >
-      <Stack width="880px" height="300px" spacing ={2} overflow={'auto'}>
+      <Box 
+      width ="800px"
+      height="100px" 
+      bgcolor={'#ADD8E6'} 
+      display={'flex'} 
+      justifyContent={'center'} 
+      alignItems={'center'}
+      border={'1px solid #ccc'}>
+        <Typography variant={'h2'} color={'#333'} textAlign={'center'} >
+           Pantry Items
+        </Typography>
+        
+      </Box>
+      <Stack width="800px" height="300px" spacing={2} overflow={'auto'}>
         {item.map((i)=>(
           <Box 
             key={i}
@@ -23,7 +38,7 @@ export default function Home() {
             alignItems={'center'}
             bgcolor={'#f0f0f0'}
           >
-            <Typography variant={'h3'} color={'#333'} textAlign={'center'} >
+            <Typography variant={'h3'} color={'black'} textAlign={'center'} >
               { 
                 // Capitalize the first letter of the item 
                 i.charAt(0).toUpperCase()+ i.slice(1)

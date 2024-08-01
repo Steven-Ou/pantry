289dd6d47e821dfb0fa3b5ed7e87ad4df942b51c
Steven-Ou <osteve425@gmail.com>
2024-08-01 13:55:39 -0400

    added width height flex centering for the box for css

--- app/page.js
+++ app/page.js
@@ -3,6 +3,12 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <Box width="100vw"></Box>
+    <Box 
+      width="100vw" 
+      height="100vw"
+      display = {'flex'}
+      justifyContent={'center'}
+      alignItems={'center'}
+    ></Box>
   );
 }

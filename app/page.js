bebe16f363cebc5e64d900e3e510c3292d741044
Steven-Ou <osteve425@gmail.com>
2024-08-01 14:44:44 -0400

    Done typography

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import {Box,Stack} from "@mui/material";
+import {Box,Stack, Typography} from "@mui/material";
 
 const item = [ 'tomato','potato','onion','garlic','carrot','lettuce','kale','cucumber']
 
@@ -12,18 +12,25 @@ export default function Home() {
       justifyContent={'center'}
       alignItems={'center'}
     >
-      <Stack width="880px" height="200px" spacing ={2} overflow={'auto'}>
+      <Stack width="880px" height="300px" spacing ={2} overflow={'auto'}>
         {item.map((i)=>(
           <Box 
             key={i}
             width="100%"
-            height="100px"
+            height="300px"
             display={'flex'}
             justifyContent={'center'}
             alignItems={'center'}
             bgcolor={'#f0f0f0'}
           >
-            {i}
+            <Typography
+            variant={'h4'}
+            color={'#333'}
+            textAlign={'center'}
+            fontWeight={'bold'}
+            >
+              {i}
+            </Typography>
           </Box>
         ))}
       </Stack>

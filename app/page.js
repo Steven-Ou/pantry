0b75bca51723ba141765191417fcec0ac28a0f5a
Steven-Ou <osteve425@gmail.com>
2024-08-01 14:30:22 -0400

    Added stack box

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import {Box} from "@mui/material";
+import {Box,Stack} from "@mui/material";
 
 const item = [ 'tomato','potato','onion','garlic','carrot']
 
@@ -12,7 +12,20 @@ export default function Home() {
       justifyContent={'center'}
       alignItems={'center'}
     >
-      Hello World
+      <Stack width="880px" height="600px" spacing ={2}>
+        {item.map((i)=>(
+          <Box 
+            key={i}
+            width="100%"
+            height="100px"
+            display={'flex'}
+            justifyContent={'center'}
+            bgcolor={'#f0f0f0'}
+          >
+            {i}
+          </Box>
+        ))}
+      </Stack>
     </Box>
   );
 }

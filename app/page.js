22016cabed16916d931ce7ae97d632abbb7acd30
Steven-Ou <osteve425@gmail.com>
2024-08-01 14:10:56 -0400

    Fixed a typo

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <Box 
       width="100vw" 
-      height="100vw"
+      height="100vh"
       display = {'flex'}
       justifyContent={'center'}
       alignItems={'center'}

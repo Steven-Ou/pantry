1fe9f899003dbd00241f7778204bc3c241cc6255
Steven-Ou <osteve425@gmail.com>
2024-08-02 14:20:13 -0400

    Addded stuff that we needed

--- app/page.js
+++ app/page.js
@@ -1,3 +1,6 @@
+'use client'
+import Image from 'next/image'
+import {useState, useEffect} from 'react'
 import {Box,Stack, Typography} from "@mui/material";
 import { lightBlue } from "@mui/material/colors";
 

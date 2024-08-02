1bf7a5364f6d2d73651004c98369b9a4e7d28520
Steven-Ou <osteve425@gmail.com>
2024-08-02 14:34:58 -0400

    added inventory

--- app/page.js
+++ app/page.js
@@ -3,11 +3,27 @@ import Image from 'next/image'
 import {useState, useEffect} from 'react'
 import {Box,Stack, Typography} from "@mui/material";
 import {firestore} from '@/firebase'
-import { lightBlue } from "@mui/material/colors";
+import { collection, getDocs, query } from 'firebase/firestore';
 
 const item = [ 'tomato','potato','onion','garlic','carrot','lettuce','kale','cucumber']
 
 export default function Home() {
+  const [inventory, setInventory]= useState([])
+  const [open, setOpen]= useState([false])
+  const [itemName, setItemName]= useState([''])
+
+  const updateInventory = async() =>{
+    const snapshot =  query(collection(firestore,'inventory'))
+    const docs = await getDocs(snapshot)
+    const inventoryList = []
+    docs.forEach((doc)=> {
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      })
+    })
+    setInventory(inventoryList)
+  }
   return (
    
     <Box 
